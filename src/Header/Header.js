726b2ab3a2bee85d9a31732834ca6a6e7290252e
devesh-agarwal <devesh.agarwal@sunarctechnologies.com>
2020-06-19 10:51:15 +0530

    sixth

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,10 +3,11 @@ import './Header.css';
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import { ThemeContext } from '../App';
+import { Hidden } from '@material-ui/core';
 
 class MainHeader extends React.Component {
     element =
-        <div className='col-md-4 MainHeaderPadding headerDiv'>
+        <div className='col-md-4 MainHeaderPadding '>
             <div className='MainHeaderElement'>
                 <div className='MainHeader'>
                     <p>{console.log(ThemeContext)}We are closed</p>
@@ -17,7 +18,8 @@ class MainHeader extends React.Component {
             </div>
         </div>
     render() {
-        return element;
+        console.log(window.screen.availWidth);
+        return this.element;
     }
 }
 
@@ -90,85 +92,95 @@ function Expandable(props) {
     )
 }
 
-
-function SubHeader() {
-    class SubHeader extends React.Component {
-
-        constructor(props) {
-            super(props);
-            this.state = { a: 'SubHeaderLabel col', b: 'SubHeaderLabel col', c: 'SubHeaderLabel col', expand: 'abc' };
-            this.handleClick = this.handleClick.bind(this);
-
-        }
-
-        handleClick(event) {
+class SubHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { a: 'SubHeaderLabel col', b: 'SubHeaderLabel col', c: 'SubHeaderLabel col', expand: 'abc' };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(event) {
+        this.setState(() => ({
+            a: 'SubHeaderLabel col',
+            b: 'SubHeaderLabel col',
+            c: 'SubHeaderLabel col',
+            expand: 'abc'
+        }));
+        if (event.currentTarget.id === 'a') {
             this.setState(() => ({
-                a: 'SubHeaderLabel col',
-                b: 'SubHeaderLabel col',
-                c: 'SubHeaderLabel col',
+                a: 'SubHeaderLabelToggle col',
                 expand: 'abc'
             }));
-            if (event.currentTarget.id === 'a') {
-                this.setState(() => ({
-                    a: 'SubHeaderLabelToggle col',
-                    expand: 'abc'
-                }));
-            } if (event.currentTarget.id === 'b') {
-                this.setState(() => ({
-                    b: 'SubHeaderLabelToggle col',
-                    expand: 'curbside'
-                }));
-            } if (event.currentTarget.id === 'c') {
-                this.setState(() => ({
-                    c: 'SubHeaderLabelToggle col',
-                    expand: 'delivery'
-                }));
-            }
+        } if (event.currentTarget.id === 'b') {
+            this.setState(() => ({
+                b: 'SubHeaderLabelToggle col',
+                expand: 'curbside'
+            }));
+        } if (event.currentTarget.id === 'c') {
+            this.setState(() => ({
+                c: 'SubHeaderLabelToggle col',
+                expand: 'delivery'
+            }));
+        }
 
-        }
-        render() {
-            return (
-                <Fragment>
-                    <div className='col-md-8'>
-                        <div className='row SubHeaderDiv headerDiv' >
-                            <label className='noMargin'>
-                                <div id='a' onClick={this.handleClick} className={this.state.a}>
-                                    <p>Takeout</p>
-                                    <img src="https://eatstax.com/static/front/images/delivery/Take_away.png" />
-                                </div>
-                            </label>
-                            <label>
-                                <div onClick={this.handleClick} id='b' className={this.state.b}>
-                                    <p>Curbside</p>
-                                    <img src="https://eatstax.com/static/front/images/delivery/Curb_Side.png" />
-                                </div>
-                            </label>
-                            <label>
-                                <div onClick={this.handleClick} id='c' className={this.state.c}>
-                                    <p>Delivery</p>
-                                    <img src="https://eatstax.com/static/front/images/delivery/Delivery.png" />
-                                </div>
-                            </label>
-                        </div>
-                    </div> {console.log(window.screen)}
-                    <Expandable expand={this.state.expand} />
-                </Fragment>
-            );
-        }
     }
-
-    return (<SubHeader />);
+    render() {
+        const oneq = this.props.backgroun;
+        return (
+            <Fragment>
+                <div className='col-md-8'>
+                    {console.log(oneq)}
+                    <div className='row SubHeaderDiv headerDiv'  >
+                        <label className='noMargin'>
+                            <div id='a' onClick={this.handleClick} className={this.state.a}>
+                                <p>Takeout</p>
+                                <img src="https://eatstax.com/static/front/images/delivery/Take_away.png" />
+                            </div>
+                        </label>
+                        <label>
+                            <div onClick={this.handleClick} id='b' className={this.state.b}>
+                                <p>Curbside</p>
+                                <img src="https://eatstax.com/static/front/images/delivery/Curb_Side.png" />
+                            </div>
+                        </label>
+                        <label>
+                            <div onClick={this.handleClick} id='c' className={this.state.c}>
+                                <p>Delivery</p>
+                                <img src="https://eatstax.com/static/front/images/delivery/Delivery.png" />
+                            </div>
+                        </label>
+                    </div>
+                </div> {console.log(window.screen)}
+                <Expandable expand={this.state.expand} />
+            </Fragment>
+        );
+    }
 }
 
 
+const HigherOrderComponent = (SubHeader) => {
+    class HOC extends React.Component {
+        render() {
+            return <SubHeader />;
+        }
+    }
+    return HOC;
+};
 
 function Header() {
-    let expand = 'delivery';
+    let expand = 'null';
+    let oneq = 'headerDiv'
+    if (window.screen.availWidth >= 760) {
+        expand = <div style={{ overflow: 'hidden' }}>
+            <img style={{ width: '100%' }} src='https://www.rd.com/wp-content/uploads/2018/12/shutterstock_1161597079.jpg' />
+        </div>
+    } else expand = null;
     return (
         <Fragment>
-            <div className='row marginZero '>
+            {expand}
+            <div className='row marginZero ' >
                 <MainHeader />
-                <SubHeader />
+                <SubHeader backgroun={oneq} />
+                {/* {HigherOrderComponent()} */}
             </div>
         </Fragment>
     )
